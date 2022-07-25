75f6f17a0466aeeea475d648d50f4e613cc5039c
Nicholas Bailey <nickcyg1992@gmail.com>
2022-07-25 16:17:42 -0500

    improvements

--- src/pages/LectureIndex/LectureIndex.jsx
+++ src/pages/LectureIndex/LectureIndex.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 const Index = (props) => {
   console.log("PROPS: ", props)
-
   return (
     <>
     {props.lectures.map(lecture => (
       <div key={lecture._id}>
         <h1>{lecture.subject}</h1>
-        <p>{lecture.takeaway}</p>
-        <p>{lecture.painpoint}</p>
+        <p>Takeaway: {lecture.takeaway}</p>
+        <p>Struggle: {lecture.painpoint}</p>
         <Link to={`/lectures/${lecture._id}` } key={lecture._id} state={{lecture}}>More Details</Link>
+        {console.log(lecture._id)}
       </div>
     ))}
     </>
